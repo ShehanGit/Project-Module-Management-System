f5b1731bb836bd5659c4f288a7166625f0ad6963
AnjanaTharanga24 <anjana24myprof@gmail.comm>
2024-03-11 19:12:33 +0530

    create add users page

---
+++ frontend/src/pages/CordinatorAddMarks.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Sidebar from "../component/Sidebar";
+
+export default function CordinatorAddMarks() {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-3">
+          <Sidebar />
+        </div>
+       
+      </div>
+    </div>
+  );
+}
