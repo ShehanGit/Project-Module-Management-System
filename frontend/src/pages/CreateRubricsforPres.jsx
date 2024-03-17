0fc31cb660684360921c2908285039645ca3e9f0
pasandu <114053092+pasandu@users.noreply.github.com>
2024-03-17 17:54:28 +0530

    add ss

--- frontend/src/pages/CreateRubricsforPres.jsx
+++ frontend/src/pages/CreateRubricsforPres.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function CreateRubricsforPres() {
   return (
     <div>
-       <h1>Marking rubrics</h1>
-       
+       <h1>Marking rubricssss</h1>
+
 
       
     </div>
